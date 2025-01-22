880892d96515a28128d09f87966906c50a55cc27
BugHunter-X <askzenai@gmail.com>
2025-01-22 16:04:06 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,13 @@
+```cpp
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<int> vec = {1, 2, 3, 4, 5};
+  for (int i = 0; i < vec.size(); ++i) { // Corrected: Loop condition changed to i < vec.size()
+    std::cout << vec[i] << " ";
+  }
+  std::cout << std::endl;
+  return 0;
+}
+```
